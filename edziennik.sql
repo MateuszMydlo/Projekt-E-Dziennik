2ccf05dced9bb19e23866c85369c8c456999db8f
FranekPieszczek <franek.pieszczek123@gmail.com>
2023-03-22 12:44:18 +0100

    dodanie haseł do tabeli students i treachers, dodanie tabeli attendance

--- edziennik.sql
+++ edziennik.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: dziennik4
+-- Host: 127.0.0.1    Database: dziennik6
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -16,6 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `attendace`
+--
+
+DROP TABLE IF EXISTS `attendace`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `attendace` (
+  `id_attendace` int NOT NULL AUTO_INCREMENT,
+  `present` tinyint(1) DEFAULT NULL,
+  `student` int DEFAULT NULL,
+  `subject` int DEFAULT NULL,
+  PRIMARY KEY (`id_attendace`),
+  KEY `student` (`student`),
+  KEY `subject` (`subject`),
+  CONSTRAINT `attendace_ibfk_1` FOREIGN KEY (`student`) REFERENCES `students` (`id_student`),
+  CONSTRAINT `attendace_ibfk_2` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id_subject`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attendace`
+--
+
+LOCK TABLES `attendace` WRITE;
+/*!40000 ALTER TABLE `attendace` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attendace` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `classes`
 --
 
@@ -28,8 +57,8 @@ CREATE TABLE `classes` (
   `subclass` char(1) DEFAULT NULL,
   `homeroom_teacher` int DEFAULT NULL,
   PRIMARY KEY (`id_class`),
-  KEY `Homeroom_Teacher` (`homeroom_teacher`),
-  CONSTRAINT `Homeroom_Teacher` FOREIGN KEY (`homeroom_teacher`) REFERENCES `teachers` (`id_teacher`)
+  KEY `homeroom_teacher` (`homeroom_teacher`),
+  CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`homeroom_teacher`) REFERENCES `teachers` (`id_teacher`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -56,10 +85,10 @@ CREATE TABLE `grades` (
   `teacher` int DEFAULT NULL,
   `student` int DEFAULT NULL,
   PRIMARY KEY (`id_grade`),
-  KEY `Graded_Students` (`student`),
-  KEY `Grading_Teacher` (`teacher`),
-  CONSTRAINT `Graded_Students` FOREIGN KEY (`student`) REFERENCES `students` (`id_student`),
-  CONSTRAINT `Grading_Teacher` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`)
+  KEY `student` (`student`),
+  KEY `teacher` (`teacher`),
+  CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student`) REFERENCES `students` (`id_student`),
+  CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -86,9 +115,10 @@ CREATE TABLE `students` (
   `pesel` int DEFAULT NULL,
   `bth_date` date DEFAULT NULL,
   `class` int DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_student`),
-  KEY `Stutents_class` (`class`),
-  CONSTRAINT `Stutents_class` FOREIGN KEY (`class`) REFERENCES `classes` (`id_class`)
+  KEY `class` (`class`),
+  CONSTRAINT `students_ibfk_1` FOREIGN KEY (`class`) REFERENCES `classes` (`id_class`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -137,6 +167,7 @@ CREATE TABLE `teachers` (
   `surname` varchar(255) DEFAULT NULL,
   `pesel` int DEFAULT NULL,
   `bth_date` date DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_teacher`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -165,10 +196,10 @@ CREATE TABLE `test_schedule` (
   `teacher` int DEFAULT NULL,
   `comment` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_test`),
-  KEY `Examiner` (`teacher`),
-  KEY `Tests_Subject:` (`subject`),
-  CONSTRAINT `Examiner` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`),
-  CONSTRAINT `Tests_Subject:` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id_subject`)
+  KEY `teacher` (`teacher`),
+  KEY `subject` (`subject`),
+  CONSTRAINT `test_schedule_ibfk_1` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`),
+  CONSTRAINT `test_schedule_ibfk_2` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id_subject`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -196,12 +227,12 @@ CREATE TABLE `timetable` (
   `day_of_the_week` varchar(255) DEFAULT NULL,
   `hour` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_entry`),
-  KEY `Timetable_Teacher` (`teacher`),
-  KEY `Timetable_Class` (`class`),
-  KEY `Timetable_Subject` (`subject`),
-  CONSTRAINT `Timetable_Class` FOREIGN KEY (`class`) REFERENCES `classes` (`id_class`),
-  CONSTRAINT `Timetable_Subject` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id_subject`),
-  CONSTRAINT `Timetable_Teacher` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`)
+  KEY `teacher` (`teacher`),
+  KEY `class` (`class`),
+  KEY `subject` (`subject`),
+  CONSTRAINT `timetable_ibfk_1` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`),
+  CONSTRAINT `timetable_ibfk_2` FOREIGN KEY (`class`) REFERENCES `classes` (`id_class`),
+  CONSTRAINT `timetable_ibfk_3` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id_subject`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -223,4 +254,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-21 22:29:02
+-- Dump completed on 2023-03-22 12:42:59
