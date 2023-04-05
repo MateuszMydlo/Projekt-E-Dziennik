aaacc360fe3aa366119ac113dd37595f29ed7cb7
FranekPieszczek <franek.pieszczek123@gmail.com>
2023-04-05 19:46:59 +0200

    dodanie maila do studentow i nauczycieli, dodanie uprawnien nauczycielom

--- Projekt Dziennik Elektroniczny 04.04 - baza w srodku/dziennik.sql
+++ Projekt Dziennik Elektroniczny 04.04 - baza w srodku/dziennik.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: dziennik
+-- Host: 127.0.0.1    Database: dzienniknowy
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `attendance`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `attendance` (
   `id_attendace` int NOT NULL AUTO_INCREMENT,
   `present` tinyint(1) DEFAULT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE `attendance` (
   KEY `subject` (`subject`),
   CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`student`) REFERENCES `students` (`id_student`),
   CONSTRAINT `attendance_ibfk_2` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id_subject`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,7 +52,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `classes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `classes` (
   `id_class` int NOT NULL AUTO_INCREMENT,
   `year` char(1) DEFAULT NULL,
@@ -61,7 +61,7 @@ CREATE TABLE `classes` (
   PRIMARY KEY (`id_class`),
   KEY `homeroom_teacher` (`homeroom_teacher`),
   CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`homeroom_teacher`) REFERENCES `teachers` (`id_teacher`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -80,7 +80,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `grades`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `grades` (
   `id_grade` int NOT NULL AUTO_INCREMENT,
   `grade` char(1) DEFAULT NULL,
@@ -93,7 +93,7 @@ CREATE TABLE `grades` (
   KEY `teacher` (`teacher`),
   CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student`) REFERENCES `students` (`id_student`),
   CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,12 +107,40 @@ INSERT INTO `grades` VALUES (1,'5','S',5,5,'2023-03-28'),(2,'2','S',5,3,'2023-03
 UNLOCK TABLES;
 
 --
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permissions` (
+  `id_perm` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `modify_students` char(1) DEFAULT NULL,
+  `modify_teachers` char(1) DEFAULT NULL,
+  `modify_timetable` char(1) DEFAULT NULL,
+  `modify_subjects` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id_perm`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` VALUES (1,'Dyrektor','1','1','1','1'),(2,'Wice Dyrektor','1','0','1','1'),(3,'Nauczyciel','0','0','0','0');
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `students`
 --
 
 DROP TABLE IF EXISTS `students`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `students` (
   `id_student` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -121,10 +149,11 @@ CREATE TABLE `students` (
   `bth_date` date DEFAULT NULL,
   `class` int DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_student`),
   KEY `class` (`class`),
   CONSTRAINT `students_ibfk_1` FOREIGN KEY (`class`) REFERENCES `classes` (`id_class`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +162,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'Marta','Marciniak','62160787265','2008-02-15',1,'MMarciniak2008'),(2,'Tadeusz','Wilk','13584726747','2008-05-16',1,'TWilk2008'),(3,'Oskar','Szulc','93113106401','2008-12-27',1,'OSzulc2008'),(4,'Maurycy','Duda','28135229714','2008-08-01',1,'MDuda2008'),(5,'Daria','Zalewska','43283074404','2008-03-17',1,'DZalewska2008'),(6,'Julianna','Kowalska','45001239153','2008-10-10',2,'JKowalska2008'),(7,'Wiktoria','Biegun','79669849757','2008-02-18',2,'WBiegun2008'),(8,'Kinga','Szulc','09755869333','2008-01-30',2,'KSzulc2008'),(9,'Alan','Wozniak','10300450230','2008-09-12',2,'AWozniak2008'),(10,'Andrzej','Witkowski','64129146227','2008-05-16',2,'AWitkowski2008'),(11,'Fryderyk','Lis','52043613797','2007-06-14',3,'FLis2007'),(12,'Emil','Maciejewski','94411107381','2007-11-30',3,'EMaciejewski2007'),(13,'Aleks','Wojciechowski','67213832056','2007-12-31',3,'AWojciechowski2007'),(14,'Daniel','Chmieleski','31012442293','2007-08-09',3,'Dchmielewski2007'),(15,'Juliusz','Jaworski','65998374344','2007-11-05',3,'JJaworski2007'),(16,'Sylwia','Sikora','77292982215','2006-02-17',4,'SSikora2006'),(17,'Kacper','Mazur','46509224964','2006-09-11',4,'KMazur2006'),(18,'Eustachy','Kondratowicz','19503029894','2006-02-28',4,'EKondratowicz2006'),(19,'Wiktor','Laskowski','63043512745','2006-04-01',4,'WLaskowicz2006'),(20,'Antoni','Kalinowski','49023047245','2006-04-02',4,'AKalinowski2006'),(21,'Krystian','Kwiatkowski','14298329789','2006-07-25',5,'KKwiatkowski2006'),(22,'Agnieszka','Majewska','75837503822','2006-09-28',5,'AMajewska'),(23,'Maksymilian','Lis','46710326791','2006-02-24',5,'MLis2006'),(24,'Monika','Dudek','02935123673','2006-05-18',5,'MDudek2006'),(25,'Olaf','Michalak','92008120787','2006-06-01',5,'OMichalak2006');
+INSERT INTO `students` VALUES (1,'Marta','Marciniak','62160787265','2008-02-15',1,'MMarciniak2008','MMarciniak2008@gmail.com'),(2,'Tadeusz','Wilk','13584726747','2008-05-16',1,'TWilk2008','TWilk2008@gmail.com'),(3,'Oskar','Szulc','93113106401','2008-12-27',1,'OSzulc2008','OSzulc2008@gmail.com'),(4,'Maurycy','Duda','28135229714','2008-08-01',1,'MDuda2008','MDuda2008@gmail.com'),(5,'Daria','Zalewska','43283074404','2008-03-17',1,'DZalewska2008','DZalewska2008@gmail.com'),(6,'Julianna','Kowalska','45001239153','2008-10-10',2,'JKowalska2008','JKowalska2008@gmail.com'),(7,'Wiktoria','Biegun','79669849757','2008-02-18',2,'WBiegun2008','WBiegun2008@gmail.com'),(8,'Kinga','Szulc','09755869333','2008-01-30',2,'KSzulc2008','KSzulc2008@gmail.com'),(9,'Alan','Wozniak','10300450230','2008-09-12',2,'AWozniak2008','AWozniak2008@gmail.com'),(10,'Andrzej','Witkowski','64129146227','2008-05-16',2,'AWitkowski2008','AWitkowski2008@gmail.com'),(11,'Fryderyk','Lis','52043613797','2007-06-14',3,'FLis2007','FLis2007@gmail.com'),(12,'Emil','Maciejewski','94411107381','2007-11-30',3,'EMaciejewski2007','EMaciejewski2007@gmail.com'),(13,'Aleks','Wojciechowski','67213832056','2007-12-31',3,'AWojciechowski2007','AWojciechowski2007@gmail.com'),(14,'Daniel','Chmieleski','31012442293','2007-08-09',3,'Dchmielewski2007','Dchmielewski2007@gmail.com'),(15,'Juliusz','Jaworski','65998374344','2007-11-05',3,'JJaworski2007','JJaworski2007@gmail.com'),(16,'Sylwia','Sikora','77292982215','2006-02-17',4,'SSikora2006','SSikora2006@gmail.com'),(17,'Kacper','Mazur','46509224964','2006-09-11',4,'KMazur2006','KMazur2006@gmail.com'),(18,'Eustachy','Kondratowicz','19503029894','2006-02-28',4,'EKondratowicz2006','EKondratowicz2006@gmail.com'),(19,'Wiktor','Laskowski','63043512745','2006-04-01',4,'WLaskowicz2006','WLaskowicz2006@gmail.com'),(20,'Antoni','Kalinowski','49023047245','2006-04-02',4,'AKalinowski2006','AKalinowski2006@gmail.com'),(21,'Krystian','Kwiatkowski','14298329789','2006-07-25',5,'KKwiatkowski2006','KKwiatkowski2006@gmail.com'),(22,'Agnieszka','Majewska','75837503822','2006-09-28',5,'AMajewska','AMajewska@gmail.com'),(23,'Maksymilian','Lis','46710326791','2006-02-24',5,'MLis2006','MLis2006@gmail.com'),(24,'Monika','Dudek','02935123673','2006-05-18',5,'MDudek2006','MDudek2006@gmail.com'),(25,'Olaf','Michalak','92008120787','2006-06-01',5,'OMichalak2006','OMichalak2006@gmail.com');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,12 +172,12 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `subjects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `subjects` (
   `id_subject` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_subject`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,7 +196,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `teachers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `teachers` (
   `id_teacher` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -175,8 +204,12 @@ CREATE TABLE `teachers` (
   `pesel` varchar(11) DEFAULT NULL,
   `bth_date` date DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_teacher`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `email` varchar(255) DEFAULT NULL,
+  `permissions` int DEFAULT NULL,
+  PRIMARY KEY (`id_teacher`),
+  KEY `permissions` (`permissions`),
+  CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`permissions`) REFERENCES `permissions` (`id_perm`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -185,7 +218,7 @@ CREATE TABLE `teachers` (
 
 LOCK TABLES `teachers` WRITE;
 /*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
-INSERT INTO `teachers` VALUES (1,'Adam','Nowak','12345678901','1990-10-23','ANowak1990'),(2,'Jan','Kowalski','21378654901','1975-05-13','JKowalski1975'),(3,'Anna','Bocian','84652487956','1993-06-29','ABocian1993'),(4,'Grzegorz','Bielik','93756385746','1988-02-04','GBielik1988'),(5,'Wojciech','Chmiel','18346836585','1995-08-10','WChmiel1995'),(6,'Andrzej','Brodnicki','84657358647','1980-12-24','ABrodnicki1980');
+INSERT INTO `teachers` VALUES (1,'Adam','Nowak','12345678901','1990-10-23','ANowak1990','ANowak1990@gmail.com',3),(2,'Jan','Kowalski','21378654901','1975-05-13','JKowalski1975','JKowalski1975@gmail.com',2),(3,'Anna','Bocian','84652487956','1993-06-29','ABocian1993','ABocian1993@gmail.com',1),(4,'Grzegorz','Bielik','93756385746','1988-02-04','GBielik1988','GBielik1988@gmail.com',3),(5,'Wojciech','Chmiel','18346836585','1995-08-10','WChmiel1995','WChmiel1995@gmail.com',3),(6,'Andrzej','Brodnicki','84657358647','1980-12-24','ABrodnicki1980','ABrodnicki1980@gmail.com',3);
 /*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -195,7 +228,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `test_schedule`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `test_schedule` (
   `id_test` int NOT NULL AUTO_INCREMENT,
   `sheduled_date` date DEFAULT NULL,
@@ -209,7 +242,7 @@ CREATE TABLE `test_schedule` (
   KEY `subject` (`subject`),
   CONSTRAINT `test_schedule_ibfk_1` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`),
   CONSTRAINT `test_schedule_ibfk_2` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id_subject`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -228,7 +261,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `timetable`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `timetable` (
   `id_entry` int NOT NULL AUTO_INCREMENT,
   `class` int DEFAULT NULL,
@@ -243,7 +276,7 @@ CREATE TABLE `timetable` (
   CONSTRAINT `timetable_ibfk_1` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id_teacher`),
   CONSTRAINT `timetable_ibfk_2` FOREIGN KEY (`class`) REFERENCES `classes` (`id_class`),
   CONSTRAINT `timetable_ibfk_3` FOREIGN KEY (`subject`) REFERENCES `subjects` (`id_subject`)
-) ENGINE=InnoDB AUTO_INCREMENT=174 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=174 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -265,4 +298,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-29 15:29:25
+-- Dump completed on 2023-04-05 19:45:44
